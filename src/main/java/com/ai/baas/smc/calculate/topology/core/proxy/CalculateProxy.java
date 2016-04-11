80036b5ddd40e11b1ce4490a19afe9c4fe5a3906
zhangbc <zhangbc@10.1.52.118>
2016-04-11 21:08:48 +0800

    提交

--- src/main/java/com/ai/baas/smc/calculate/topology/core/proxy/CalculateProxy.java
+++ src/main/java/com/ai/baas/smc/calculate/topology/core/proxy/CalculateProxy.java
@@ -12,26 +12,32 @@ import org.wltea.expression.datameta.Variable;
 import com.ai.baas.dshm.client.CacheFactoryUtil;
 import com.ai.baas.dshm.client.impl.DshmClient;
 import com.ai.baas.dshm.client.interfaces.IDshmClient;
+import com.ai.baas.smc.api.elementmanage.interfaces.IElementManageSV;
+import com.ai.baas.smc.api.elementmanage.param.ElementSearchResponseVO;
 import com.ai.baas.smc.api.policymanage.interfaces.IPolicyManageSV;
 import com.ai.baas.smc.api.policymanage.param.PolicyDetailQueryConditionInfo;
 import com.ai.baas.smc.api.policymanage.param.PolicyDetailQueryItemInfo;
+import com.ai.baas.smc.api.policymanage.param.PolicyDetailQueryPlanInfo;
 import com.ai.baas.smc.api.policymanage.param.PolicyDetailQueryRequest;
 import com.ai.baas.smc.api.policymanage.param.PolicyDetailQueryResponse;
 import com.ai.baas.smc.api.policymanage.param.PolicyListQueryRequest;
 import com.ai.baas.smc.api.policymanage.param.PolicyListQueryResponse;
+import com.ai.baas.smc.api.policymanage.param.StepCalValue;
 import com.ai.baas.smc.calculate.topology.core.util.CacheBLMapper;
 import com.ai.baas.smc.calculate.topology.core.util.IKin;
 import com.ai.opt.sdk.util.DubboConsumerFactory;
 import com.ai.paas.ipaas.mcs.interfaces.ICacheClient;
+
 /**
  * 
  * @author zhangbc
  *
  */
-public class CalculateProxy{
+public class CalculateProxy {
 
 	/**
 	 * 根据对象类型获取该对象下有效政策
+	 * 
 	 * @param objectId
 	 * @param tenantId
 	 * @return
@@ -49,6 +55,7 @@ public class CalculateProxy{
 
 	/**
 	 * 获取政策下面的所有结算项
+	 * 
 	 * @param policyId
 	 * @param tenantId
 	 * @return
@@ -91,70 +98,145 @@ public class CalculateProxy{
 
 	/**
 	 * 是否匹配规则
+	 * 
 	 * @param stream
 	 * @param paramList
 	 * @param policyItemList
 	 * @return
 	 */
 	public boolean matchPolicy(String[] stream, List paramList, List<PolicyDetailQueryItemInfo> policyItemList) {
-		// TODO Auto-generated method stub
-		List valueList=valueList(stream,paramList);
-		String compare="";
-		boolean flag=true;
-		for(PolicyDetailQueryItemInfo policyDetailQueryItemInfo:policyItemList)
-		{
-		 List<PolicyDetailQueryConditionInfo> policyConditionList=policyDetailQueryItemInfo.getPolicyDetailQueryConditionInfos();
-		 for(PolicyDetailQueryConditionInfo policyDetailQueryConditionInfo:policyConditionList)
-		 {
-			String matchType= policyDetailQueryConditionInfo.getMatchType();
-			String matchValue=policyDetailQueryConditionInfo.getMatchValue();
-		if(matchType.equals("in"))
-		{
-			flag=IKin.in(matchValue, compare);
-			
+		Map map = valueMap(stream, paramList);
+
+		boolean flag = true;
+		IElementManageSV elementManageSV = DubboConsumerFactory.getService(IElementManageSV.class);
+		for (PolicyDetailQueryItemInfo policyDetailQueryItemInfo : policyItemList) {
+			List<PolicyDetailQueryConditionInfo> policyConditionList = policyDetailQueryItemInfo
+					.getPolicyDetailQueryConditionInfos();
+			for (PolicyDetailQueryConditionInfo policyDetailQueryConditionInfo : policyConditionList) {
+				String matchType = policyDetailQueryConditionInfo.getMatchType();
+				String matchValue = policyDetailQueryConditionInfo.getMatchValue();
+				Long elementId = policyDetailQueryConditionInfo.getElementId();
+				ElementSearchResponseVO elementSearchResponseVO = elementManageSV.searchElementById(elementId);
+				long sortId = elementSearchResponseVO.getSortId();
+				int num = (int) sortId;
+				String compare = stream[num];
+
+				if (matchType.equals("in")) {
+					flag = IKin.in(matchValue, compare);
+
+				} else if (matchType.equals("nin")) {
+					flag = !IKin.in(matchValue, compare);
+				} else {
+					String expression = "a" + matchType + "b";
+					List<Variable> variables = new ArrayList<Variable>();
+					variables.add(Variable.createVariable("a", compare));
+					variables.add(Variable.createVariable("b", matchValue));
+					Object result = ExpressionEvaluator.evaluate(expression, variables);
+					flag = Boolean.parseBoolean(result.toString());
+				}
+
+			}
+
 		}
-		else if(matchType.equals("nin"))
-		{
-			flag=!IKin.in(matchValue, compare);
-		}
-		else
-		{
-			String expression="a"+matchType+"b";
-			 List<Variable> variables = new ArrayList<Variable>();
-			    variables.add(Variable.createVariable("a", compare));
-			    variables.add(Variable.createVariable("b",matchValue));
-			    Object result = ExpressionEvaluator.evaluate(expression, variables);
-			    flag=Boolean.parseBoolean(result.toString());
-		}
-		
-		 }
-		 
-		}
-		
+
 		return flag;
 	}
-	
-	
-	
-	public List valueList(String [] stream,List paramList)
-	{
-		List<Map<String,String>> valueList=new ArrayList<Map<String,String>>();
-		for(int i=0;i<stream.length;i++)
-		{
-			Map map=new HashMap();
-			for(int j=0;j<paramList.size();j++)
-			{
-				Map paramMap=(Map) paramList.get(j);
-				if(paramMap.containsKey(i))
-				{
-					map.put(paramMap.get(i), stream[i])	;
+
+	public Map valueMap(String[] stream, List paramList) {
+		Map map = new HashMap();
+		for (int i = 0; i < stream.length; i++) {
+			for (int j = 0; j < paramList.size(); j++) {
+				Map paramMap = (Map) paramList.get(j);
+				if (paramMap.containsKey(i)) {
+					map.put(paramMap.get(i), stream[i]);
 				}
 			}
-			valueList.add(map);
 		}
-		return valueList;
-		
+		return map;
+
 	}
-	
+
+	public void caculateFees(List<PolicyDetailQueryItemInfo> list, String[] stream) {
+		for (PolicyDetailQueryItemInfo policyDetailQueryItemInfo : list) {
+			List<PolicyDetailQueryPlanInfo> planInfoList = policyDetailQueryItemInfo.getPolicyDetailQueryPlanInfos();
+			for (PolicyDetailQueryPlanInfo policyDetailQueryPlanInfo : planInfoList) {
+
+			}
+		}
+	}
+
+	/**
+	 * 算费
+	 * 
+	 * @param planType
+	 * @param calType
+	 * @return
+	 */
+	public double docaculate(PolicyDetailQueryPlanInfo policyDetailQueryPlanInfo, String[] stream) {
+		double value = 0;
+		IElementManageSV elementManageSV = DubboConsumerFactory.getService(IElementManageSV.class);
+		String planType = policyDetailQueryPlanInfo.getPlanType();
+		String calType = policyDetailQueryPlanInfo.getCalType();// 算费方式
+		Long elementId = policyDetailQueryPlanInfo.getElementId();
+		
+		ElementSearchResponseVO elementSearchResponseVO = elementManageSV.searchElementById(elementId);
+		long sortId = elementSearchResponseVO.getSortId();
+		int num = (int) sortId;
+		String compare = stream[num];
+		if (planType.equals("nomal")) {
+			double calValue=Double.parseDouble(policyDetailQueryPlanInfo.getNormalCalValue());
+			if(calType.equals("ratio"))
+			{
+				value=Double.parseDouble(compare)*calValue;	
+			}
+			else if(calType.equals("fixed"))
+			{
+				value=calValue;
+			}
+			else if(calType.equals("price"))
+			{
+				value=Double.parseDouble(compare)*calValue;		
+			}
+			
+		} else if (planType.equals("step")) {
+			List<StepCalValue> list = policyDetailQueryPlanInfo.getStepCalValues();
+			String calValue = "";
+			for (StepCalValue stepCalValue : list) {
+				double start = Double.parseDouble(stepCalValue.getStartValue());
+				double end = Double.parseDouble(stepCalValue.getEndValue());
+				if(end<value&&start<value)
+				{
+					calValue = stepCalValue.getCalValue();
+					value+=Double.parseDouble(calValue)*(end-start);	
+				}
+				if (value > start && value < end) {
+					calValue = stepCalValue.getCalValue();
+					value+=Double.parseDouble(calValue)*(Double.parseDouble(compare)-start);
+				}
+				
+			}
+		} else if ("switch".equals(planType)) {
+
+			List<StepCalValue> list = policyDetailQueryPlanInfo.getStepCalValues();
+			String calValue = "";
+			for (StepCalValue stepCalValue : list) {
+				double start = Double.parseDouble(stepCalValue.getStartValue());
+				double end = Double.parseDouble(stepCalValue.getEndValue());
+				if (Double.parseDouble(compare) > start && Double.parseDouble(compare) < end) {
+					calValue = stepCalValue.getCalValue();
+				}
+			}
+
+			if (calType.equals("ratio")) {
+				value = Double.parseDouble(compare) * Double.parseDouble(calValue);
+			} else if (calType.equals("fixed")) {
+				value = Double.parseDouble(calValue);
+			} else if (calType.equals("price")) {
+				value = Double.parseDouble(compare) * Double.parseDouble(calValue);
+			}
+		}
+
+		return value;
+	}
 
 }

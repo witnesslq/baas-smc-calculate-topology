799995166e2610ed721fcb3374f6652809b967e5
zhangbc <zhangbc@10.1.52.118>
2016-04-21 17:41:27 +0800

    tt

--- src/main/java/com/ai/baas/smc/calculate/topology/core/flow/CalFeesFlow.java
+++ src/main/java/com/ai/baas/smc/calculate/topology/core/flow/CalFeesFlow.java
@@ -30,25 +30,12 @@ public class CalFeesFlow extends BaseFlow {
 	@Override
 	@SuppressWarnings("unchecked")
 	public void define() {
-		TopologyBuilder topologyBuilder = new TopologyBuilder();
         // 设置喷发节点并分配并发数，该并发数将会控制该对象在集群中的线程数。
-        topologyBuilder.setSpout("SimpleSpout", new CalSpout(), 1);
+		builder.setSpout("SimpleSpout", new CalSpout(), 1);
         // 设置数据处理节点并分配并发数。指定该节点接收喷发节点的策略为随机方式。
-        topologyBuilder.setBolt("SimpleBolt", new CostCalculatingBolt(), 3).shuffleGrouping("SimpleSpout");
+		builder.setBolt("SimpleBolt", new CostCalculatingBolt(), 3).shuffleGrouping("SimpleSpout");
 		
-        Config config = new Config();
-        config.setDebug(true);
-            config.setNumWorkers(1);
-            try {
-				StormSubmitter.submitTopology("", config, topologyBuilder.createTopology());
-			} catch (AlreadyAliveException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InvalidTopologyException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
+      
 		
 		
 	}

a3796df7d609acd22a49a5123d11c4ad35df43af
myto2006 <myto2006@126.com>
2016-06-08 14:41:47 +0800

    log

--- src/main/java/com/ai/baas/smc/calculate/topology/core/flow/CalFeesFlow.java
+++ src/main/java/com/ai/baas/smc/calculate/topology/core/flow/CalFeesFlow.java
@@ -7,29 +7,23 @@ import com.ai.baas.smc.calculate.topology.core.bolt.CostCalculatingBolt;
 import com.ai.baas.smc.calculate.topology.core.spout.CalSpout;
 import com.ai.baas.storm.flow.BaseFlow;
 
-
-/**
- * GPRS通用拓扑图
- * @author majun
- * @since 2016.3.16
- */
 public class CalFeesFlow extends BaseFlow {
-	private static Logger logger = LoggerFactory.getLogger(CalFeesFlow.class);
-	
-	@Override
-	@SuppressWarnings("unchecked")
-	public void define() {
+    private static Logger logger = LoggerFactory.getLogger(CalFeesFlow.class);
+
+    @Override
+    public void define() {
         // 设置喷发节点并分配并发数，该并发数将会控制该对象在集群中的线程数。
-		builder.setSpout("SimpleSpout", new CalSpout(), 1);
+        builder.setSpout("SimpleSpout", new CalSpout(), 1);
         // 设置数据处理节点并分配并发数。指定该节点接收喷发节点的策略为随机方式。
-		builder.setBolt("SimpleBolt", new CostCalculatingBolt(), getParallelNum("smc.calculate.executor.num",1)).shuffleGrouping("SimpleSpout");
-		
-	}
+        builder.setBolt("SimpleBolt", new CostCalculatingBolt(),
+                getParallelNum("smc.calculate.executor.num", 1)).shuffleGrouping("SimpleSpout");
 
-	public static void main(String[] args) {
-		CalFeesFlow flow = new CalFeesFlow();
-		flow.run(args);
-	}
+    }
 
-	
+    public static void main(String[] args) {
+        logger.info("开始启动算费拓扑...");
+        CalFeesFlow flow = new CalFeesFlow();
+        flow.run(args);
+    }
+
 }
--- src/main/java/com/ai/baas/smc/calculate/topology/core/spout/CalSpout.java
+++ src/main/java/com/ai/baas/smc/calculate/topology/core/spout/CalSpout.java
@@ -34,82 +34,91 @@ import com.ai.opt.sdk.components.mcs.MCSClientFactory;
 import com.ai.paas.ipaas.mcs.interfaces.ICacheClient;
 import com.alibaba.fastjson.JSON;
 
-public class CalSpout extends BaseRichSpout{
-	private static final long serialVersionUID = 5296876971073823644L;
-	private static Logger logger = LoggerFactory.getLogger(CalSpout.class);
-	private SpoutOutputCollector collector;
-	// private static Connection connection;
-	
-	@Override
-	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
-		//System.out.println("open--------------");
-		HBaseProxy.loadResource(conf);
-		HBaseProxy.getConnection();
-		loadCacheResource(conf);
-		this.collector = collector;
-	}
-	
-	private void loadCacheResource(Map<String,String> config){
-		Properties p=new Properties();
-		p.setProperty(SmcConstants.PAAS_AUTH_URL, config.get(SmcConstants.PAAS_AUTH_URL));
-		p.setProperty(SmcConstants.PAAS_AUTH_PID, config.get(SmcConstants.PAAS_AUTH_PID));
-		p.setProperty(SmcConstants.PAAS_CCS_SERVICEID, config.get(SmcConstants.PAAS_CCS_SERVICEID));
-		p.setProperty(SmcConstants.PAAS_CCS_SERVICEPASSWORD, config.get(SmcConstants.PAAS_CCS_SERVICEPASSWORD));
-		//OptConfHelper.loadPaaSConf(p);
-		ComponentConfigLoader.loadPaaSConf(p);
-	}
+public class CalSpout extends BaseRichSpout {
+    private static final long serialVersionUID = 5296876971073823644L;
 
-	@Override
-	public void nextTuple() {
-		ICacheClient cacheStatsTimes = MCSClientFactory.getCacheClient(SmcCacheConstant.NameSpace.STATS_TIMES);
-		String finishlist = cacheStatsTimes.get(SmcCacheConstant.Cache.finishKey);
-		if(StringUtils.isBlank(finishlist)){
-			return;
-		}
-		List<FinishListVo> voList = JSON.parseArray(finishlist,FinishListVo.class);
-		System.out.println("[收到统计数]--->>"+voList.size());
-		cacheStatsTimes.del(SmcCacheConstant.Cache.finishKey);
-		for (FinishListVo vo : voList) {
-			int count = 0;
-			String tenantId = vo.getTenantId();
-			String batchNo = vo.getBatchNo();
-			String billTimeSn = vo.getBillTimeSn();
-			//String billTimeSn = "201605";
-			String objectId = vo.getObjectId();
-			System.out.println("[账期]--->>"+billTimeSn);
-			System.out.println("[批次号]--->>"+batchNo);
-			System.out.println("[objectId]--->>"+objectId);
-			cacheStatsTimes.hset(SmcCacheConstant.Cache.lockKey, batchNo, vo.getStats_times());
-			try {
-				Table table = HBaseProxy.getConnection().getTable(TableName.valueOf("RTM_OUTPUT_DETAIL_" + billTimeSn));
-				Scan scan = new Scan();
-				scan.setFilter(new SingleColumnValueFilter("bsn".getBytes(),
-						"value".getBytes(), CompareFilter.CompareOp.EQUAL,
-						new BinaryComparator(batchNo.getBytes())));
-				ResultScanner rs = table.getScanner(scan);
-				for (Result r : rs) {// 按行去遍历
-					String line = "";
-					for (KeyValue kv : r.raw()) {// 遍历每一行的各列
-						if (Bytes.toString(kv.getQualifier()).equals("record")) {
-							line = Bytes.toString(kv.getValue());
-							//System.out.println("---"+line);
-							collector.emit(new Values(line,objectId));
-						}
-					}
-					count++;
-				}
-				rs.close();
-				table.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			System.out.println("从副本库[RTM_OUTPUT_DETAIL_" + billTimeSn+"]中取出"+count+"条数据!");
-		}
-	}
+    private static Logger logger = LoggerFactory.getLogger(CalSpout.class);
 
-	@Override
-	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		 declarer.declare(new Fields("line","objectId"));
-	}
+    private SpoutOutputCollector collector;
+
+    // private static Connection connection;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void open(@SuppressWarnings("rawtypes")
+    Map conf, TopologyContext context, SpoutOutputCollector collector) {
+        // System.out.println("open--------------");
+        HBaseProxy.loadResource(conf);
+        HBaseProxy.getConnection();
+        loadCacheResource(conf);
+        this.collector = collector;
+    }
+
+    private void loadCacheResource(Map<String, String> config) {
+        Properties p = new Properties();
+        p.setProperty(SmcConstants.PAAS_AUTH_URL, config.get(SmcConstants.PAAS_AUTH_URL));
+        p.setProperty(SmcConstants.PAAS_AUTH_PID, config.get(SmcConstants.PAAS_AUTH_PID));
+        p.setProperty(SmcConstants.PAAS_CCS_SERVICEID, config.get(SmcConstants.PAAS_CCS_SERVICEID));
+        p.setProperty(SmcConstants.PAAS_CCS_SERVICEPASSWORD,
+                config.get(SmcConstants.PAAS_CCS_SERVICEPASSWORD));
+        // OptConfHelper.loadPaaSConf(p);
+        ComponentConfigLoader.loadPaaSConf(p);
+    }
+
+    @Override
+    public void nextTuple() {
+        ICacheClient cacheStatsTimes = MCSClientFactory
+                .getCacheClient(SmcCacheConstant.NameSpace.STATS_TIMES);
+        String finishlist = cacheStatsTimes.hget(SmcCacheConstant.NameSpace.STATS_TIMES,
+                SmcCacheConstant.Cache.finishKey);
+        if (StringUtils.isBlank(finishlist)) {
+            return;
+        }
+        List<FinishListVo> voList = JSON.parseArray(finishlist, FinishListVo.class);
+        System.out.println("[收到统计数]--->>" + voList.size());
+        cacheStatsTimes.hdel(SmcCacheConstant.NameSpace.STATS_TIMES,
+                SmcCacheConstant.Cache.finishKey);
+        for (FinishListVo vo : voList) {
+            int count = 0;
+            String tenantId = vo.getTenantId();
+            String batchNo = vo.getBatchNo();
+            String billTimeSn = vo.getBillTimeSn();
+            // String billTimeSn = "201605";
+            String objectId = vo.getObjectId();
+            System.out.println("[账期]--->>" + billTimeSn);
+            System.out.println("[批次号]--->>" + batchNo);
+            System.out.println("[objectId]--->>" + objectId);
+            cacheStatsTimes.hset(SmcCacheConstant.Cache.lockKey, batchNo, vo.getStats_times());
+            try {
+                Table table = HBaseProxy.getConnection().getTable(
+                        TableName.valueOf("RTM_OUTPUT_DETAIL_" + billTimeSn));
+                Scan scan = new Scan();
+                scan.setFilter(new SingleColumnValueFilter("bsn".getBytes(), "value".getBytes(),
+                        CompareFilter.CompareOp.EQUAL, new BinaryComparator(batchNo.getBytes())));
+                ResultScanner rs = table.getScanner(scan);
+                for (Result r : rs) {// 按行去遍历
+                    String line = "";
+                    for (KeyValue kv : r.raw()) {// 遍历每一行的各列
+                        if (Bytes.toString(kv.getQualifier()).equals("record")) {
+                            line = Bytes.toString(kv.getValue());
+                            // System.out.println("---"+line);
+                            collector.emit(new Values(line, objectId));
+                        }
+                    }
+                    count++;
+                }
+                rs.close();
+                table.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("从副本库[RTM_OUTPUT_DETAIL_" + billTimeSn + "]中取出" + count + "条数据!");
+        }
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("line", "objectId"));
+    }
 
 }
